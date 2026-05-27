c2b85af25a87c3fe89bf8b977ca85f48818d0b30
abhisheksamari-hash <abhishek.samari@paytm.com>
2026-05-27 15:58:12 +0530

    check commiy

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -21,6 +21,7 @@ export function AuthProvider({ children }) {
     return firebaseSignOut(auth);
   }
 
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
